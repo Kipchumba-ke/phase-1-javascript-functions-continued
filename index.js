fde65392f69f6be7ecc1ca6edf79dd6b57e7892e
Kelvin Kipchumba <chogekelvin90@gmail.com>
2024-10-23 12:17:36 +0300

    created function declaration, expression and IIFE

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // code your solution here
+//Function declaration
+function saturdayFun(activity = "roller-skate"){
+    return `This Saturday, I want to ${activity}!`;
+}
+
+const mondayWork = function(activit = 'go to the office'){
+    return `This Monday, I will ${activit}.`
+}
+
+function wrapAdjective(wraps = `*`){
+    return function (msg = `special` ) {
+        return `You are ${wraps}${msg}${wraps}!`
+    }
+}
